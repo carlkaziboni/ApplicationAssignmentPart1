4203431980cf0d384b26c6d9728590896e484793
carlkaziboni <carlkazibonicman@hotmail.com>
2024-03-27 20:08:55 +0000

    ToDo2

--- src/todo02_assignments/Assignment.java
+++ src/todo02_assignments/Assignment.java
@@ -5,6 +5,7 @@ import java.time.Duration;
 import java.time.ZonedDateTime;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Collections;
 
 public class Assignment {
     /* Compulsory information */
@@ -14,12 +15,13 @@ public class Assignment {
 
     /* Optional information */
     // The name of the assignment, but it can have no name
-    private final String name = null; // TODO The initialisation of name needs altering
+    private final String name; // TODO The initialisation of name needs altering
 
     /* TODO (Much) later, try adding at least one more piece of optional
             information to test your design's flexibility, and how simple you
             can keep the constructor's parameters.
      */
+    private final String course;
 
 
     /* TODO Optional: check how you can stop other classes manipulating this
@@ -28,14 +30,32 @@ public class Assignment {
             data. This exaple is here just to prove a point.
      */
     private final List<String> toProveAPoint = new ArrayList<>();
-    private void addNameDefaultNames(){
-        // TODO Delete this method when you delete toProveAPoint
-        for ( String name : new String[]{"Fiona", "Vid", "Daniel"} ){
-            toProveAPoint.add(name);
-        }
-    }
 
     public Assignment(ZonedDateTime endDate,
+    ZonedDateTime startDate,
+    BigDecimal hoursRequired, String name, String course) {
+        // Separate parameters per item works but is not a neat technique
+
+        this.endDate       = endDate;
+        this.startDate     = startDate;
+        this.hoursRequired = hoursRequired;
+        this.name          = name;
+        this.course        = course;
+}
+
+    public Assignment(ZonedDateTime endDate,
+                      ZonedDateTime startDate,
+                      BigDecimal hoursRequired, String name) {
+        // Separate parameters per item works but is not a neat technique
+
+        this.endDate       = endDate;
+        this.startDate     = startDate;
+        this.hoursRequired = hoursRequired;
+        this.name          = name;
+        this.course        = "Course Name";
+    }
+
+   public Assignment(ZonedDateTime endDate,
                       ZonedDateTime startDate,
                       BigDecimal hoursRequired) {
         // Separate parameters per item works but is not a neat technique
@@ -43,28 +63,29 @@ public class Assignment {
         this.endDate       = endDate;
         this.startDate     = startDate;
         this.hoursRequired = hoursRequired;
-
-        addNameDefaultNames(); // TODO Delete this call
+        this.name          = "Assignment";
+        this.course        = "Course Name";
     }
-
+     
     // getters for immutable objects can safely return the original object
 
     public ZonedDateTime getStartDate() {return startDate;}
     public String getName() {return name;}
     public ZonedDateTime getEndDate() {return endDate;}
+    public String getCourse() {return course;}
 
     /* TODO This getter is potentially unsafe,
             though BigDecimal has no public methods for altering its state,
             so don't worry about it. Getters for primitive types are safe.
      */
-    public BigDecimal getHoursRequired() {return hoursRequired;}
+    public long getHoursRequired() {return hoursRequired.longValueExact();}
 
 
     // TODO This getter is definitely unsafe: can you make it safe?
-    public List<String> getToProveAPoint() {return toProveAPoint;}
+    public List<String> getToProveAPoint() {return Collections.unmodifiableList(toProveAPoint);}
 
     public Duration getDuration(){
-        // TODO Calculate the duration
-        return Duration.ofDays(7); // 7 days left: loads of time!
+        Duration duration = Duration.between(startDate, endDate);
+        return duration; // 7 days left: loads of time!
     }
 }
